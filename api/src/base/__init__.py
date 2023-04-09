38e46706766faea275d0e894277c0722bc366509
noctisdark <diseven@outlook.fr>
2023-04-09 06:44:52 +0200

    add CORS

--- api/src/base/__init__.py
+++ api/src/base/__init__.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
@@ -6,4 +8,8 @@ from .config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Need this extension because Chromes blocks all requests  even on localhost
+if os.getenv('DEBUG'): CORS(app)
+
 db = SQLAlchemy(app)

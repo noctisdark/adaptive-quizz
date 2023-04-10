f88721bdb608d5719da958c0b146b5a3f13e6a50
noctisdark <diseven@outlook.fr>
2023-04-10 13:48:18 +0200

    add navigation

--- front/src/components/Home/index.js
+++ front/src/components/Home/index.js
@@ -1,19 +1,22 @@
 import { Button } from "@chakra-ui/react";
+import Navigation from "components/Navigation";
+
 import UserProvider, { useUser } from "providers/UserProvider";
 
 const Hello = () => {
-  const {user} = useUser();
-  return `Hello ${user.username}.`
-}
+  const { user } = useUser();
+  return `Hello ${user.username}.`;
+};
 
 const LogoutButton = () => {
   const { logout } = useUser();
-  return <Button onClick={logout}>Logout</Button>
-}
+  return <Button onClick={logout}>Logout</Button>;
+};
 
 const Home = () => {
   return (
     <UserProvider>
+      <Navigation />
       <Hello />
       <LogoutButton />
     </UserProvider>
---
+++ front/src/components/Navigation/Banner.js
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+import { Stack, Text } from "@chakra-ui/react";
+
+const Logo = styled.img`
+  max-height: 75%;
+`;
+
+const Banner = () => (
+  <Stack height="100%" direction="row" alignItems="center">
+    <Logo src="/logo.svg" alt="Apdative Quiz's logo" />
+    <Text as="b">Adaptive Quiz</Text>
+  </Stack>
+);
+
+export default Banner;
